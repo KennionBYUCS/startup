0ba51d81219f7fbf747ad00620f3ecba8b0a4547
kennion <kennion@byu.edu>
2024-03-09 12:40:51 -0700

    Fix duplicate error message bug

--- login.js
+++ login.js
@@ -1,6 +1,10 @@
+let errorMessageElement;
+
 function login() {
     const formElement = document.querySelector("#form");
     // checking to see if username/password combination stored in local storage matches input
+    // as of right now, this will never happen, but this will later be replaced by a database call
+    // that checks the stored username and password combinations
     if (localStorage.getItem("name") != null 
         && (localStorage.getItem("name") === document.querySelector("#name").value)) {
         if (localStorage.getItem("password") != null 
@@ -9,16 +13,20 @@ function login() {
             return;
         } else {
             if (!formElement.hasAttribute("login_wrong_password_error_message")) {
-                removeErrorTypes();
-                formElement.append("Incorrect password");
+                clearErrorMessage();
+                errorMessageElement = document.createElement("div");
+                errorMessageElement.textContent = "Incorrect password";
+                formElement.appendChild(errorMessageElement);
                 formElement.setAttribute("login_wrong_password_error_message", "true");
             }
         }
     } 
 
     if (!formElement.hasAttribute("login_unknown_account_error_message")) {
-        removeErrorTypes();
-        formElement.append("Unregistered account");
+        clearErrorMessage();
+        errorMessageElement = document.createElement("div");
+        errorMessageElement.textContent = "Unregistered account";
+        formElement.appendChild(errorMessageElement);
         formElement.setAttribute("login_unknown_account_error_message", "true");
     }
 }
@@ -31,8 +39,10 @@ function create_account() {
     if (nameElement.value === "" || passwordElement.value === "") {
         // could also add regex types to see if username/password are reasonable
         if (!formElement.hasAttribute("account_creation_error_message")) {
-            removeErrorTypes();
-            formElement.append("Please input both a username and password");
+            clearErrorMessage();
+            errorMessageElement = document.createElement("div");
+            errorMessageElement.textContent = "Please input both a username and password";
+            formElement.appendChild(errorMessageElement);
             formElement.setAttribute("account_creation_error_message", "true");
         }
         return;
@@ -43,8 +53,13 @@ function create_account() {
     window.location.href = "select.html";
 }
 
-function removeErrorTypes() {
+function clearErrorMessage() {
     const formElement = document.querySelector("#form");
+
+    if (errorMessageElement) {
+        formElement.removeChild(errorMessageElement);
+        errorMessageElement = null;
+    }
 
     formElement.removeAttribute("account_creation_error_message");
     formElement.removeAttribute("login_wrong_password_error_message");
